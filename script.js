28a299d10924826bdb20b624f1a8d8405b652ac1
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 17:22:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
-    //Write your code here
-  
+              let radius = document.getElementById('radius');
+              let volume = document.getElementById('volume');
+              let radiusValue = radius.value;
+              let volumeValue = (4/3)*Math.PI*radiusValue**3;
+              volume.value= volumeValue; 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
