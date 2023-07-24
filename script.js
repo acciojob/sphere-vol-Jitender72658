c9cfe5072ea1131b8bead591f66bbc5dcd4f9cd7
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 17:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ function volume_sphere() {
               let volumeValue = (4/3)*Math.PI*radiusValue**3;
               volume.value= volumeValue; 
 } 
-
+let form = document.getElementById('MyForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+  // Add your form handling logic here
+});
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
